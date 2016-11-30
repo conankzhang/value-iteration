50ddaf440c1dac8b2fc3bbabd7a3d4a2d485409f
ConanZhang <conankzhang@gmail.com>
2016-11-29 21:46:01 -0700

    temp

--- CS4300_MDP_value_iteration.m
+++ CS4300_MDP_value_iteration.m
@@ -48,34 +48,34 @@ iter = 0;
 
 while(done == 0 && iter < max_iter)
     iter = iter + 1;
+    U_trace(end+1) = U_prime;
     U = U_prime;
     delta = 0;
     
     s_length = length(S);
     
-    % Do we need 2 s loops
     for s1 = 1:s_length
-       for s2 = 1:s_length
-           best_action = 0;
-           best_val = -Inf;
+       best_action = 0;
+       best_val = -Inf;
 
-           a_length = length(A);
-           for a = 1:a_length
+       a_length = length(A);
+       for a = 1:a_length
+          for s2 = 1:s_length
               a_sum = 0;
 
               s_prime_length = P(s2,a).probs;
               for s_prime = 1:s_prime_length
                  a_sum = a_sum + P(s2,a).probs(s_prime) * U(s_prime);
-                 
+
                  if(a_sum > best_val)
                     best_action = a; 
                  end
               end
-           end
-
-           pi_star(s2) = best_action;
-       end 
-        
+              
+              pi_star(s2) = best_action;
+          end
+       end
+ 
        U_prime(s1) = R(s1) + (gamma * max(pi_star));
        
        if(abs(U_prime(s1) - U(s1)) > delta)
